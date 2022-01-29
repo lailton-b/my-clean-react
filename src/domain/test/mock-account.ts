3802be8a5ca943bef460afdf6fec9ef2edee6516
Lailton <lailton_b@hotmail.com>
2022-01-29 11:52:19 -0300

    test: ensure remoAuthentication returns an Account on success

--- src/domain/test/mock-account.ts
+++ src/domain/test/mock-account.ts
@@ -1,8 +1,14 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { AuthenticationParams } from '@/domain/usecases/authentication'
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { AccountModel } from '../models/account-model'
 import faker from 'faker'
 
 export const mockAuthentication = (): AuthenticationParams => ({
   email: faker.internet.email(),
   password: faker.internet.password()
+})
+
+export const mockAccountModel = (): AccountModel => ({
+  accessToken: faker.random.uuid()
 })
